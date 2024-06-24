446e2a4897f79c84bcd257baedee7f5d7280133e
San <85775871+disaa0@users.noreply.github.com>
2024-06-24 00:59:57 -0400

    deploy api

--- aws/api_gateway/api_gateway_stage.tf
+++ aws/api_gateway/api_gateway_stage.tf
@@ -1,6 +1,6 @@
 resource "aws_api_gateway_stage" "test-stage" {
   cache_cluster_enabled = "false"
-  deployment_id         = "cooreh"
+  deployment_id         = "deploy1"
   rest_api_id           = "4su6xysix1"
   stage_name            = "test"
   xray_tracing_enabled  = "false"
